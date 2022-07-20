bbf9188cd55500e0183ea3fa83d6b218ac228d60
Marek Sobolak <Marek.Sobolak@dolby.com>
2022-07-20 17:44:46 +0200

    CCS-3401 Remove files left by Web SDK 3.7.0-beta.2 release in GitHub repository + map file from 3.3.0.

--- types/services/video/LocalVideoTrack.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @ignore
- */
-export declare class LocalVideoTrack {
-    protected mediaStreamTrack: MediaStreamTrack;
-    protected constructor(track: MediaStreamTrack);
-    getMediaStreamTrack(): MediaStreamTrack;
-    stop(): Promise<void>;
-}
